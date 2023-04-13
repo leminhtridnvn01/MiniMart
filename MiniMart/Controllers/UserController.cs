14637acfd64c59675d7b945c5d584c3ee030c157
tri_le <tri_le@datahouse.com>
2023-04-13 22:19:20 +0700

    Init Controller Change Port IIS

---
+++ MiniMart/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiniMart.API.Controllers
+{
+    [ApiController]
+    public class UserController
+    {
+        public UserController() { }
+        [HttpGet("current-user")]
+        public bool Get()
+        {
+            return true;
+        }
+    }
+}

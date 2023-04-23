e18d623a0533fd0391454e0662c2a656bc062b8a
leminhtridnvn01 <81277223+leminhtridnvn01@users.noreply.github.com>
2023-04-23 10:31:08 +0700

    Add Unit column for Product (#6)
    Co-authored-by: tri_le <tri_le@datahouse.com>

--- MiniMart.Infrastructure/Migrations/20230423032940_Add-Unit-Column-for-Product.Designer.cs
+++ MiniMart.Infrastructure/Migrations/20230423032940_Add-Unit-Column-for-Product.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MiniMart.Infrastructure.Data;
 
@@ -11,9 +12,10 @@ using MiniMart.Infrastructure.Data;
 namespace MiniMart.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230423032940_Add-Unit-Column-for-Product")]
+    partial class AddUnitColumnforProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
